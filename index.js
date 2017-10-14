e521aae0af3b8e360fceda7cc7bd04e365f34e45
arve0 <arve.seljebu@gmail.com>
2017-10-14 02:32:51 +0200

    use api

--- index.js
+++ index.js
@@ -1,120 +1,57 @@
 #!/usr/bin/env node
-const path = require('path')
-const store = require('node-persist')
-const pacote = require('pacote')
-const request = require('./request')
-const url = require('url')
-const prefix = require('si-prefix')
-const dots = require('cli-spinners').dots.frames
-let prevSpin = Date.now() - 1000
-spin()
+const https = require('https')
 
-let cache = path.join(process.env.APPDATA || process.env.HOME, '.npm', '_cacache')
+let spinners = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏']
+let interval = setInterval(() => {
+  let c = spinners.shift()
+  process.stdout.write('\b')  // backspace
+  process.stdout.write(c)
+  spinners.push()
+}, 100)
 
-store.initSync({
-  dir: path.join(__dirname, '.cache')
-})
+main()
 
 async function main () {
   if (process.argv.length < 3 || process.argv[2] === '--help') {
-    console.log('Usage: download-size package-name')
-    console.log('Clear cache: download-size --clear-cache')
-  } else if (process.argv[2] === '--clear-cache') {
-    store.clearSync()
+    console.log('Usage: download-size package-name [another-package ...]')
   } else {
     for (let i = 2; i < process.argv.length; i++) {
-      let result = await getSize(process.argv[i])
-      console.log(`${result.name}: ${result.size}`)
+      try {
+        let result = await request(process.argv[i])
+        console.log(`${result.name}@${result.version}: ${result.prettySize}`)
+      } catch (err) {
+        console.error('' + err)
+      }
     }
   }
-}
-main().catch((err) => {
-  console.error('' + err)
-  process.exit(1)
-})
-
-async function resolve (pkgName, version, resolved = {}) {
-  version = version || 'latest'
-  let manifest = await pacote.manifest(`${pkgName}@${version}`, { cache })
-  spin()
-
-  let depVer = `${pkgName}@${manifest.version}`
-  if (depVer in resolved) {
-    return resolved
-  }
-  resolved[depVer] = manifest._resolved
-
-  let pending = []
-  let deps = manifest.dependencies || {}
-  for (let dep in deps) {
-    pending.push(resolve(dep, deps[dep], resolved))
-  }
-  await Promise.all(pending)
-
-  return { depVer, pkgs: resolved }
+  clearInterval(interval)
 }
 
-async function getSize (pkgName) {
-  let resolved = await resolve(pkgName)
-
-  let pending = []
-  for (let pkg in resolved.pkgs) {
-    pending.push(getTarballSize(resolved.pkgs[pkg]))
+function request (pkgName) {
+  const options = {
+    hostname: 'api.seljebu.no',
+    path: '/download-size/' + pkgName,
+    method: 'GET'
   }
-  let sizes = await Promise.all(pending)
+  const req = https.request(options)
+  req.end()
 
-  return {
-    name: resolved.depVer,
-    size: prettyPrint(sum(sizes))
-  }
-}
+  return new Promise((resolve, reject) => {
+    req.on('response', (response) => {
+      let body = ''
+      response.setEncoding('utf8')
+      response.on('data', (chunk) => {
+        body += chunk
+      })
 
-function sum (arr) {
-  return arr.reduce((s, i) => s + i, 0)
-}
-
-async function getTarballSize (tarballUrl) {
-  let size = await store.getItem(tarballUrl)
-  spin()
-
-  if (typeof size === 'number') {
-    return size
-  }
-
-  let { hostname, path } = url.parse(tarballUrl)
-  const response = await request.head({
-    hostname,
-    path
+      response.on('end', () => {
+        if (response.statusCode !== 200) {
+          // use error message from server
+          reject(new Error(`${body}`))
+          return
+        }
+        resolve(JSON.parse(body))
+      })
+    })
   })
-
-  if (!response.headers['content-length']) {
-    throw new Error(`Did not get content length for ${tarballUrl}`)
-  }
-
-  size = parseInt(response.headers['content-length'])
-
-  if (typeof size !== 'number' || !isFinite(size)) {
-    throw new Error(`Unable to parse content-length ${response.headers['content-length']} of ${tarballUrl}`)
-  }
-
-  store.setItem(tarballUrl, size)
-  return size
-}
-
-function prettyPrint (size) {
-  let converted = prefix.byte.convert(size)
-  process.stdout.write('\b') // remove spinner
-  let output = converted[0].toFixed(2) + ' ' + converted[1]
-  return output
-}
-
-function spin () {
-  if ((Date.now() - prevSpin) < 100) {
-    return
-  }
-  prevSpin = Date.now()
-  let c = dots.shift()
-  dots.push(c)
-  process.stdout.write('\b')
-  process.stdout.write(c)
 }
